f2b36148ce0b71681ac5808078b88922ed61d523
bmraubo <bmraubo@gmail.com>
2021-11-09 11:02:17 +0000

    added dummy test

---
+++ src/test/java/TestDummy.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestDummy {
+
+    @Test
+    void testDummy() {
+        Assertions.assertEquals(true,true);
+    }
+}
