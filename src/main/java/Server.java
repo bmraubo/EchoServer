61ddce1d83d20f76822499c0dc9c9e31e7b83ca4
bmraubo <bmraubo@gmail.com>
2021-11-09 16:53:52 +0000

    added input/output stream

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,12 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
     public static void main(String[] args) throws Exception{
         int port = 5000;
-        ServerSocket socket = new ServerSocket(port);
+        ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("server started");
 
+        while (true) {
+            //set up client socket
+            Socket socket = serverSocket.accept();
+            System.out.println("connection made");
+            //open IO stream
+            InputStreamReader inputStream = new InputStreamReader(socket.getInputStream());
+            BufferedReader input = new BufferedReader(inputStream);
+            PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
+            //wait for data
+
+            //do something with the data
+
+            //close IO streams then socket
+        }
     }
 
 }
