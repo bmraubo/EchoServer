a0751c2cc0a4492e7ddf9da6e534013021faa81e
bmraubo <bmraubo@gmail.com>
2021-11-17 15:29:08 +0000

    made prepareResponse method public

--- src/main/java/MethodNotAllowed.java
+++ src/main/java/MethodNotAllowed.java
@@ -1,6 +1,6 @@
 public class MethodNotAllowed {
 
-    private static Response prepareResponse(String[] allowedMethods) {
+    public static Response prepareResponse(String[] allowedMethods) {
         Response response = new Response();
         System.out.println("Method not allowed!");
         response.setStatusCode(405);
