f78f7ba78a3be485086002514412191f5e42e95f
bmraubo <bmraubo@gmail.com>
2021-11-17 10:13:28 +0000

    implemented simple head to simple get resource

--- src/main/java/SimpleGet.java
+++ src/main/java/SimpleGet.java
@@ -1,8 +1,26 @@
 public class SimpleGet {
 
     public static Response prepareResponse(Request request) {
+        if (request.method.equals("GET")) {
+            return simpleGet(request);
+        } else if (request.method.equals("HEAD")) {
+            return simpleHead(request);
+        } else {
+            return null; // wrong method placeholder
+        }
+
+    }
+
+    private static Response simpleGet(Request request) {
         Response response = new Response();
         System.out.println("Simple Get Identified");
+        response.setStatusCode(200);
+        return response;
+    }
+
+    private static Response simpleHead(Request request) {
+        Response response = new Response();
+        System.out.println("Simple Head Identified");
         response.setStatusCode(200);
         return response;
     }
