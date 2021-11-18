e6e8b8fa042095f1fc155da0192ac518ce807703
bmraubo <bmraubo@gmail.com>
2021-11-18 12:15:31 +0000

    added prepareResponse to MethodOptions2

--- src/main/java/MethodOptions2.java
+++ src/main/java/MethodOptions2.java
@@ -1,3 +1,12 @@
 public class MethodOptions2 {
     static String[] allowedMethods = {"GET", "HEAD", "OPTIONS", "PUT", "POST"};
+
+    public static Response prepareResponse(Request request) {
+        Response response = new Response();
+        System.out.println("Simple Options to Method Options Identified");
+        response.setStatusCode(200);
+        response.setAllowHeader(allowedMethods);
+        response.addResponseBody("");
+        return response;
+    }
 }
