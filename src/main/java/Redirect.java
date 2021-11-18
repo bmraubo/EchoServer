deaa0f6b47bf82df902cd13e04e4997edce4a6fa
bmraubo <bmraubo@gmail.com>
2021-11-18 16:47:06 +0000

    added redirect endpoint

---
+++ src/main/java/Redirect.java
@@ -0,0 +1,19 @@
+public class Redirect implements RoutingInterface{
+    String[] allowedMethods = {"GET"};
+    String contentLocation = "http://0.0.0.0/simple_get";
+    String method;
+
+    public Redirect(String method) {
+        this.method = method;
+    }
+
+    @Override
+    public Response prepareResponse() {
+        Response response = new Response();
+        System.out.println("Redirecting");
+        response.setStatusCode(301);
+        response.setContentLocationHeader(contentLocation);
+        response.addResponseBody("");
+        return response;
+    }
+}
