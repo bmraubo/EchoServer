496d59e1cffc6ba6c7ae2107df7a99cadb99361f
bmraubo <bmraubo@gmail.com>
2021-11-18 14:35:49 +0000

    Routing interface template method

--- src/main/java/RoutingInterface.java
+++ src/main/java/RoutingInterface.java
@@ -1,20 +1,4 @@
 public interface RoutingInterface {
 
-    static Response routeConnection(Request request) {
-        switch (request.uri) {
-            case ("/simple_get"):
-                return SimpleGet.prepareResponse(request);
-            case ("/simple_get_with_body"):
-                return SimpleGetWithBody.prepareResponse(request);
-            case ("/echo_body"):
-                return EchoBody.prepareResponse(request);
-            case ("/head_request"):
-                return HeadRequest.prepareResponse(request);
-            case ("/method_options"):
-                return MethodOptions.prepareResponse(request);
-            case ("/method_options2"):
-                return MethodOptions2.prepareResponse(request);
-        }
-        return ResourceNotFound.prepareResponse(request);
-    }
+    Response prepareResponse();
 }
