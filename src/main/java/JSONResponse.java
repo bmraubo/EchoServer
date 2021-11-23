6dc423a1fd4eb02186fbb56bc8a535ceb6057295
bmraubo <bmraubo@gmail.com>
2021-11-23 13:48:34 +0000

    added charset to test and implementation

--- src/main/java/JSONResponse.java
+++ src/main/java/JSONResponse.java
@@ -2,7 +2,7 @@ import org.json.JSONObject;
 
 public class JSONResponse implements RoutingInterface{
     JSONObject responseBody = generateJSONResponse();
-    String contentType = "application/json";
+    String contentType = "application/json;charset=utf-8";
 
     @Override
     public Response prepareResponse() {
