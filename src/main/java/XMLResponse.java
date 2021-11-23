4a54e1b289f903608c5c2619f9f82454d752946f
bmraubo <bmraubo@gmail.com>
2021-11-23 15:24:30 +0000

    added contentType to XML response

--- src/main/java/XMLResponse.java
+++ src/main/java/XMLResponse.java
@@ -1,8 +1,11 @@
 public class XMLResponse implements RoutingInterface{
+    String contentType = "application/xml;charset=utf-8";
+
     @Override
     public Response prepareResponse() {
         Response response = new Response();
         response.setStatusCode(200);
+        response.setContentType(contentType);
         return response;
     }
 }
