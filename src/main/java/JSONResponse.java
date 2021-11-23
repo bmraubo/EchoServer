28b05de5416b444a91afa017dc225974339b8d09
bmraubo <bmraubo@gmail.com>
2021-11-23 13:47:33 +0000

    implemented json response endpoint

---
+++ src/main/java/JSONResponse.java
@@ -0,0 +1,21 @@
+import org.json.JSONObject;
+
+public class JSONResponse implements RoutingInterface{
+    JSONObject responseBody = generateJSONResponse();
+    String contentType = "application/json";
+
+    @Override
+    public Response prepareResponse() {
+        Response response = new Response();
+        response.setStatusCode(200);
+        response.setContentType(contentType);
+        response.addResponseBody(responseBody.toString());
+        return response;
+    }
+
+    private JSONObject generateJSONResponse() {
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("body", "json");
+        return jsonResponse;
+    }
+}
