868cdde9a5dc70df677313614fd458bc5895beb7
bmraubo <bmraubo@gmail.com>
2021-11-23 15:21:43 +0000

    Test XML Response status code added

---
+++ src/test/java/TestXMLResponse.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestXMLResponse {
+
+    @Test
+    void XMLResponseTest() {
+        XMLResponse testResponse = new XMLResponse();
+        Response testResponseObject = testResponse.prepareResponse();
+
+        String expectedXMLString = "<body><response>Hello World</response></body>";
+
+        Assertions.assertEquals(200, testResponseObject.statusCode);
+    }
+}
