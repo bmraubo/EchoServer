0985b882e2b55d289d3be5716f824268fa6684b4
bmraubo <bmraubo@gmail.com>
2021-11-25 14:22:35 +0000

    Replaced Response with New Response

--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -1,126 +1,17 @@
 public class Response {
-    String crlf = "\r\n";
-    // Response Line
-    String protocol = "HTTP/1.1";
-    int statusCode;
-    String reasonPhrase;
+    ResponseBuilder responseBuilder;
 
-    //Headers
-    String contentLength;
-    boolean closeConnection = true;
-    String closeConnectionHeader = "Connection: close";
-    String allowedMethods;
-    String locationHeader;
-    String contentType;
+    String responseLine;
+    String responseHeaders;
+    byte[] responseBody;
 
-    // Body
-    boolean sendBody = false;
-    String body;
-    byte[] imageBody;
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-        determineReasonPhrase(statusCode);
+    public Response(ResponseBuilder responseBuilder) {
+        this.responseBuilder = responseBuilder;
     }
 
-    /*
-    public String generateResponseString() {
-        System.out.println("generating response");
-        String responseString;
-        String responseLine = generateResponseLine();
-        String responseHeaders = generateHeaders();
-        responseString = responseLine + responseHeaders;
-        if (sendBody) {
-            String responseBody = crlf + body;
-            responseString = responseString + responseBody;
-        }
-        System.out.println(responseString);
-        return responseString;
-    }
-    */
-
-    public String generateResponseLine() {
-        return this.protocol + " " + this.statusCode + " " + this.reasonPhrase + crlf;
-    }
-
-    public String generateHeaders() {
-        String headers = "";
-        headers = headers + closeConnectionHeader + crlf;
-        if (allowedMethods != null) {
-            headers = headers + allowedMethods + crlf;
-        }
-        if (locationHeader != null) {
-            headers = headers + locationHeader + crlf;
-        }
-        if (contentType != null) {
-            headers = headers + contentType + crlf;
-        }
-        if (sendBody) {
-            headers = headers + contentLength + crlf;
-        }
-        headers = headers + crlf;
-        return headers;
-    }
-
-    private void determineReasonPhrase(int statusCode) {
-        switch (statusCode) {
-            case (200):
-                this.reasonPhrase = "OK";
-                break;
-            case (301):
-                this.reasonPhrase = "Moved Permanently";
-                break;
-            case (404):
-                this.reasonPhrase = "Not Found";
-                break;
-            case (405):
-                this.reasonPhrase = "Method Not Allowed";
-                break;
-            case (500):
-                this.reasonPhrase = "Internal Server Error";
-                break;
-        }
-    }
-
-    public void addResponseBody(String body) {
-        System.out.println("Adding Response Body");
-        if (body != null) {
-            contentLength = calculateContentLength(body);
-            this.body = body;
-            this.sendBody = true;
-        }
-    }
-
-    public void addResponseBody(byte[] image) {
-        if (image != null) {
-            contentLength = calculateContentLength(image);
-            this.imageBody = image;
-            this.sendBody = true;
-        }
-    }
-
-    private String calculateContentLength(String body) {
-        return "Content-Length: " + String.valueOf(body.length());
-    }
-
-    private String calculateContentLength(byte[] image) {
-        return "Content-Length: " + String.valueOf(image.length);
-    }
-
-    public void setAllowHeader(String[] allowedMethods) {
-        System.out.println("Setting Allow Header");
-        String methods = "Allow: ";
-        for (String x : allowedMethods) {
-            methods = methods + x + ", ";
-        }
-        this.allowedMethods = methods.substring(0, methods.length()-2);
-    }
-
-    public void setLocationHeader(String location) {
-        this.locationHeader = "Location: " + location;
-    }
-
-    public void setContentType(String contentType) {
-        this.contentType = "Content-Type: " + contentType;
+    public void generateResponse() {
+        this.responseLine = responseBuilder.getResponseLine();
+        this.responseHeaders = responseBuilder.getHeaders();
+        this.responseBody = responseBuilder.getBody();
     }
 }
--- src/test/java/TestResponse.java
+++ src/test/java/TestResponse.java
@@ -1,128 +1,29 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+
 public class TestResponse {
 
     @Test
-    void set200StatusCodeTest() {
+    void generateResponseTest() {
         int statusCode = 200;
+        String testBody = "Hello World";
 
-        Response testResponse = new Response();
-        testResponse.setStatusCode(statusCode);
+        TextResponseBuilder textResponseBuilder = new TextResponseBuilder();
+        Response response = new Response(textResponseBuilder);
 
-        Assertions.assertEquals(200, testResponse.statusCode);
-        Assertions.assertEquals("HTTP/1.1 200 OK\r\n", testResponse.generateResponseLine());
-    }
+        textResponseBuilder.setStatusCode(statusCode);
+        textResponseBuilder.setResponseBody(testBody);
 
-    @Test
-    void set301StatusCodeTest() {
-        int statusCode = 301;
+        response.generateResponse();
 
-        Response testResponse = new Response();
-        testResponse.setStatusCode(statusCode);
+        String expectedResponseLine = "HTTP/1.1 200 OK\r\n";
+        String expectedHeaders = "Content-Length: 11\r\n\r\n";
+        byte[] expectedBody = testBody.getBytes(StandardCharsets.UTF_8);
 
-        Assertions.assertEquals(301, testResponse.statusCode);
-        Assertions.assertEquals("Moved Permanently", testResponse.reasonPhrase);
-    }
-
-    @Test
-    void set404StatusCodeTest() {
-        int statusCode = 404;
-
-        Response testResponse = new Response();
-        testResponse.setStatusCode(statusCode);
-
-        Assertions.assertEquals(404, testResponse.statusCode);
-        Assertions.assertEquals("Not Found", testResponse.reasonPhrase);
-    }
-
-    @Test
-    void set500StatusCodeTest() {
-        int statusCode = 500;
-
-        Response testResponse = new Response();
-        testResponse.setStatusCode(statusCode);
-
-        Assertions.assertEquals(500, testResponse.statusCode);
-        Assertions.assertEquals("Internal Server Error", testResponse.reasonPhrase);
-    }
-
-    @Test
-    void addResponseBodyTest() {
-        int statusCode = 200;
-        String responseBody = "Hello World";
-
-        Response testResponse = new Response();
-        testResponse.setStatusCode(statusCode);
-        testResponse.addResponseBody(responseBody);
-
-        Assertions.assertEquals("Content-Length: 11", testResponse.contentLength);
-        Assertions.assertEquals("Hello World", testResponse.body);
-        Assertions.assertTrue(testResponse.sendBody);
-
-    }
-
-    @Test
-    void addResponseBodyByteTest() {
-        int statusCode = 200;
-        byte[] responseBody = new byte[3];
-
-        Response testResponse = new Response();
-        testResponse.setStatusCode(statusCode);
-        testResponse.addResponseBody(responseBody);
-
-        Assertions.assertEquals("Content-Length: 3", testResponse.contentLength);
-        Assertions.assertTrue(testResponse.sendBody);
-
-    }
-
-    @Test
-    void setAllowHeaderTest() {
-        String[] allowedMethods = {"GET", "HEAD", "OPTIONS"};
-
-        Response testResponse = new Response();
-        testResponse.setAllowHeader(allowedMethods);
-
-        String expectedHeader = "Allow: GET, HEAD, OPTIONS";
-
-        Assertions.assertEquals(expectedHeader, testResponse.allowedMethods);
-    }
-
-    @Test
-    void setLocationTest() {
-        String location = "http://content.location";
-
-        Response testResponse = new Response();
-        testResponse.setLocationHeader(location);
-
-        String expectedLocationHeader = "Location: http://content.location";
-
-        Assertions.assertEquals(expectedLocationHeader, testResponse.locationHeader);
-    }
-
-    @Test
-    void generateResponseLineTest() {
-        Response testResponse = new Response();
-        testResponse.setStatusCode(200);
-
-        String testResponseString = testResponse.generateResponseLine();
-
-        String expectedResponseString = "HTTP/1.1 200 OK\r\n";
-
-        Assertions.assertEquals(expectedResponseString, testResponseString);
-    }
-
-    @Test
-    void setContentTypeTest() {
-        String contentType = "text/plain";
-
-        Response testResponse = new Response();
-        testResponse.setStatusCode(200);
-        testResponse.setContentType(contentType);
-        testResponse.addResponseBody("");
-
-        String expectedContentTypeHeader = "Content-Type: text/plain";
-
-        Assertions.assertEquals(expectedContentTypeHeader, testResponse.contentType);
+        Assertions.assertEquals(expectedResponseLine, response.responseLine);
+        Assertions.assertEquals(expectedHeaders, response.responseHeaders);
+        Assertions.assertArrayEquals(expectedBody, response.responseBody);
     }
 }
