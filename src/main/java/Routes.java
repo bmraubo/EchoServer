d9875897c44f20290e3b4a3a1365099c62f33e75
bmraubo <bmraubo@gmail.com>
2021-11-29 19:29:49 +0000

    used class method to implement routes

--- src/main/java/Routes.java
+++ src/main/java/Routes.java
@@ -2,21 +2,21 @@ public class Routes {
 
     static Router assignRoutes() {
         Router router = new Router();
-        router.routes.put("/simple_get", new SimpleGet());
-        router.routes.put("/simple_get_with_body", new SimpleGetWithBody());
-        router.routes.put("/echo_body", new EchoBody());
-        router.routes.put("/head_request", new HeadRequest());
-        router.routes.put("/method_options", new MethodOptions());
-        router.routes.put("/method_options2", new MethodOptions2());
-        router.routes.put("/redirect", new Redirect());
-        router.routes.put("/text_response", new TextResponse());
-        router.routes.put("/html_response", new HTMLResponse());
-        router.routes.put("/json_response", new JSONResponse());
-        router.routes.put("/xml_response", new XMLResponse());
-        router.routes.put("/health-check.html", new HealthCheck());
-        router.routes.put("/kitteh.jpg", new Kitteh());
-        router.routes.put("/doggo.png", new Doggo());
-        router.routes.put("/kisses.gif", new Kisses());
+        router.addRoute("/simple_get", new SimpleGet());
+        router.addRoute("/simple_get_with_body", new SimpleGetWithBody());
+        router.addRoute("/echo_body", new EchoBody());
+        router.addRoute("/head_request", new HeadRequest());
+        router.addRoute("/method_options", new MethodOptions());
+        router.addRoute("/method_options2", new MethodOptions2());
+        router.addRoute("/redirect", new Redirect());
+        router.addRoute("/text_response", new TextResponse());
+        router.addRoute("/html_response", new HTMLResponse());
+        router.addRoute("/json_response", new JSONResponse());
+        router.addRoute("/xml_response", new XMLResponse());
+        router.addRoute("/health-check.html", new HealthCheck());
+        router.addRoute("/kitteh.jpg", new Kitteh());
+        router.addRoute("/doggo.png", new Doggo());
+        router.addRoute("/kisses.gif", new Kisses());
         return router;
     }
 }
