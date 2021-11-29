ed070a4557ad74f85d8db2f1bfe8e40f554b7f23
bmraubo <bmraubo@gmail.com>
2021-11-29 14:38:18 +0000

    added header extraction method

--- src/main/java/RequestBuilder.java
+++ src/main/java/RequestBuilder.java
@@ -1,12 +1,60 @@
-public class RequestBuilder {
-    String statusLine;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 
-    public void extractRequest(String requestData){
-        String[] requestArray = requestData.split("\r\n");
-        extractStatusLine(requestArray);
+public class RequestBuilder {
+    String[] requestArray;
+    String statusLine;
+    LinkedHashMap<String, String> headers;
+    boolean requestIncludesBody;
+
+    public RequestBuilder(){
+        generateHeaderMap();
     }
 
-    private void extractStatusLine(String[] requestArray) {
+    public void extractRequest(String requestData){
+        requestArray = requestData.split("\r\n");
+        extractStatusLine();
+        requestIncludesBody = checkForBody();
+    }
+
+    private void extractStatusLine() {
         statusLine = requestArray[0];
+    }
+
+    private void extractHeaders() {
+        if (requestIncludesBody) {
+            for (String x : Arrays.copyOfRange(requestArray, 1, requestArray.length -1)) {
+                String headerKey = extractHeaderKey(x);
+                String headerValue = extractHeaderValue(x);
+                headers.put(headerKey, headerValue);
+            }
+        } else {
+            for (String x : Arrays.copyOfRange(requestArray, 1, requestArray.length -1)) {
+                String headerKey = extractHeaderKey(x);
+                String headerValue = extractHeaderValue(x);
+                headers.put(headerKey, headerValue);
+            }
+        }
+    }
+
+    private String extractHeaderKey(String header) {
+        return header.substring(0, header.indexOf(":"));
+    }
+
+    private String extractHeaderValue(String header) {
+        return header.substring(header.indexOf(" ") + 1, header.length());
+    }
+
+    private void generateHeaderMap() {
+        headers = new LinkedHashMap<String, String>();
+    }
+
+    private boolean checkForBody() {
+        for (String x : requestArray) {
+            if (x.startsWith("Content-Length")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
