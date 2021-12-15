b42ce154e81ec718e6a9be88cbc078721f61d4ab
bmraubo <bmraubo@gmail.com>
2021-12-15 13:57:55 +0000

    rolled back changes to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 import site.bmraubo.http_server.Router;
 import site.bmraubo.http_server.Server;
-import site.bmraubo.todo.PostgresTaskList;
-import site.bmraubo.todo.Task;
 
 import java.io.IOException;
 
@@ -9,13 +7,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         int port = 5000;
-
-        PostgresTaskList tasksTest = new PostgresTaskList();
-        Task testTask = new Task("{\"task\":\"a task\"}");
-        tasksTest.addTask(testTask);
-        tasksTest.viewTaskByID(4);
-        tasksTest.updateTask(4,"{\"task\":\"a new task\"}");
-        tasksTest.removeTask(6);
 
         Router router = Routes.assignRoutes();
         Server server = new Server(router);
